f327f47a51c246a084ab57cc784e7434aa57e58f
Iordan Pinto <pintoiordan@gmail.com>
2024-07-01 18:20:21 +0200

    read more added

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function toggleReadMore() {
+  const readMoreLink = document.querySelector('.read-more');
+  const moreText = document.querySelector('.more-text');
+
+  if (moreText.style.display === "none") {
+    moreText.style.display = "inline";
+    readMoreLink.innerHTML = " read less";
+  } else {
+    moreText.style.display = "none";
+    readMoreLink.innerHTML = "...read more ";
+  }
+}
